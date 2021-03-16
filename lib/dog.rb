6f175cb08be28a5896535b4f1ee05538b03946d1
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-16 11:21:10 -0700

    commit tree a0ac97e68f2634b40bb1b28034571b7ff4b45b38

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save
+        
+    end
+
+    #a class method returns all dog instances
+    def self.all
+        @@all
+        
+    end
+
+    #a class method that empties the @@all array of all existing dogs
+    def self.clear_all
+        @@all.clear
+    end
+
+    #a class method that puts out the name of each dog to the terminal
+    def self.print_all
+        @@all.each {|n| puts "#{n.name}"}
+    end
+
+    def save
+        @@all << self
+    end
+end
